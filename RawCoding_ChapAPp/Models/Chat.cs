f492d7e010ba43fdf3b97d139f87a503c5a62a2f
Farhan Kaioum <km1472233@gmail.com>
2020-11-15 13:01:20 +0600

    ViewComponent Added

--- RawCoding_ChapAPp/Models/Chat.cs
+++ RawCoding_ChapAPp/Models/Chat.cs
@@ -32,3 +32,4 @@ namespace RawCoding_ChapAPp.Models
         public DateTime Timestamp { get; set; }
     }
 }
+
---
+++ RawCoding_ChapAPp/ViewComponents/RoomViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RawCoding_ChapAPp.Data;
+using System.Linq;
+
+namespace RawCoding_ChatApp.ViewComponents
+{
+    public class RoomViewComponent : ViewComponent
+    {
+        private ApplicationDbContext _ctx;
+
+        public RoomViewComponent(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var chats = _ctx.Chats.ToList();
+            return View(chats);
+        }
+    }
+}
